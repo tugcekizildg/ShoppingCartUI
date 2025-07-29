319889ec0377287eed999439e1affb9cc02a62a8
Tuğçe Kızıldağ <tugce.kutuk88@gmail.com>
2025-07-29 21:03:24 +0300

    base files and base functionality added

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,24 @@
+const ProductCard = ({ product }) => {
+  return (
+    <div className='bg-white p-4 rounded-lg shadow-md border-l-4 border-blue-400 flex flex-col'>
+      <img
+        src={product.image}
+        alt={product.name}
+        className='h-60 object-cover mb-4'
+      />
+      <h2 className='text-xl font-bold text-gray-600 mb-1'>{product.name}</h2>
+      <p className='text-sm text-gray-600 mb-2 font-semibold'>
+        {product.description}
+      </p>
+      <p className='text-sm text-gray-600'>
+        <strong className='text-blue-400'>Category:</strong> {product.category}
+      </p>
+      <p className='text-sm text-gray-600'>
+        <strong className='text-blue-400'>Price:</strong> $
+        {product.price.toFixed(2)}
+      </p>
+    </div>
+  );
+};
+
+export default ProductCard;
